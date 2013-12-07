4a72064a27f354ecf7de9e5e896aa89a7048aab5
Jeremy Sweet <Jeremys@fullsail.edu>
2013-12-06 19:44:55 -0500

    Cant get my food2fork api to work...

--- js/main.js
+++ js/main.js
@@ -3,46 +3,45 @@
 // Project 2 6 Dec 13
 $('#pageInstagram').on('pageinit', function(){
 	$(function() {
-		var tag = "kittens";
+		var tag = "Food";
 		var myid = "872dc4b5f50f42e1af5505f454e12af2";
 		var url = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=" + myid + "&amp;min_id=10";
-		$.getJSON(url, screenOutput);
+		$.getJSON(url, instagramData);
 	});
 	
-	var screenOutput = function(info) {
-		console.log(info);	
+	var instagramData = function(pictureInfo) {
+		console.log(pictureInfo);	
 	
-		$("#data-msg").html("Instagram results:");
+		$("#data-message").html("Instagram results:");
 		
-		//<img src='{url}' alt='{caption}' /><p>{fullname}, <em>{username}</em></p>
-		
-		$.each(info.data, function (index, photo) {
-			var pic = "<img src='" + photo.images.standard_resolution.url + "' alt='" + photo.user.id + "' /><p>" + photo.user.full_name + ", <em>" + photo.user.username + "</em></p>";
-			$("#data-output").append(pic);
-		$("#instagramResults").append(pic);
+		$.each(pictureInfo.data, function (index, photo) {
+		var pictureStuff = "<li><img src='" + photo.images.standard_resolution.url + "' alt='" + photo.user.id + "' /><p>" + photo.user.full_name + ", <em>" + photo.user.username + "</em></p></li>";
+		$("#instagramResults").append(pictureStuff);
 		});
 	};
 });
 
 $('#pageFood2Fork').on('pageinit', function(){
+
 	$(function() {
-		var tag = "Banana";
+		var tag = "banana";
 		var myid = "675668d6b35b46c525aa967347be024c";
-		var url = "https://api.instagram.com/v1/tags/" + tag + "&q=" + tag;
-		$.getJSON(url, screenOutput);
+		var url = "http://food2fork.com/api/search?key=" + myid + "&q=" + tag + "&count=1";
+		console.log(url);
+// "http://food2fork.com/api/search?key=675668d6b35b46c525aa967347be024c&q=banana&count=1"
+
+		$.getJSON(url, food2ForkData);
 	});
+		$("#data-message").html("Food2Fork results:");
 	
-	var screenOutput = function(info) {
-		console.log(info);	
+	var food2ForkData = function(recipesInfo) {
+		alert("test");
+		console.log(recipesInfo);	
 	
-		//$("#data-msg").html("Food Results");
 		
-		//<img src='{url}' alt='{caption}' /><p>{fullname}, <em>{username}</em></p>
-		
-		//$.each(info.data, function (index, photo) {
-		//	var pic = "<img src='" + photo.images.standard_resolution.url + "' alt='" + photo.user.id + "' /><p>" + photo.user.full_name + ", <em>" + photo.user.username + "</em></p>";
-		//	$("#data-output").append(pic);
-		//$("#instagramResults").append(pic);
-		//});
+		$.each(recipesInfo.data, function (index, recipes) {
+		var recipesStuff = "<li>" + recipes.title + "</li>"
+		$("#food2ForkResults").append(recipesStuff);
+		});
 	};
 });
